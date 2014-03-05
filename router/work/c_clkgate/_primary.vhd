583c4cf0f6db57ebc3a53e00236ee5ef810f7c58
happy baoliang <happybaoliang@gmail.com>
2014-03-04 22:25:56 -0500

    third

--- router/work/c_clkgate/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity c_clkgate is
-    port(
-        clk             : in     vl_logic;
-        active          : in     vl_logic;
-        clk_gated       : out    vl_logic
-    );
-end c_clkgate;
