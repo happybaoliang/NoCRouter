e92954eff4c5a0c63c2f901b91767bf8d6de61c9
happybaoliang <happybaoliang@gmail.com>
2014-05-12 21:27:16 -0400

    integrated version: free_buffer_tracker+memory_bank.
    the width of 'vc_read_from' and 'vc_written_into' are modified, each wire dedicates to a specified vc ID.
    
    the testbench is also modified, but still some work to do to make the functionality of this dynamically allocated VC works well.
    
    the 'make run' is also modified to adopt this integration.

--- DynVCRouter/memory_bank.v
+++ DynVCRouter/memory_bank.v
@@ -1,4 +1,4 @@
-module memory_bank(clk,reset,flit_in,vc_written_into,flit_out,vc_read_from);
+module memory_bank(clk,reset,vc_written_into,flit_in,write_enable,vc_read_from,flit_out,read_enable,memory_bank_full,memory_bank_empty);
 `include "c_functions.v"
 
 
@@ -9,14 +9,18 @@ parameter memory_bank_width=64;	// in bits
 parameter memory_bank_depth=32;	// in flits
 
 
-localparam pointer_depth=max_vc_number;
+localparam vc_pointer_width=clogb(max_vc_number);
 
-localparam pointer_width=clogb(memory_bank_depth);
+localparam memory_addr_width=clogb(memory_bank_depth);
 
 
 input clk;
 
 input reset;
+
+input read_enable;
+
+input write_enable;
 
 input [0:memory_bank_width-1] flit_in;
 
@@ -27,93 +31,118 @@ input [0:max_vc_number-1] vc_written_into;
 output [0:memory_bank_width-1] flit_out;
 reg [0:memory_bank_width-1] flit_out;
 
+output memory_bank_full;
+wire memory_bank_full;
+
+output memory_bank_empty;
+wire memory_bank_empty;
+
 
 reg [0:memory_bank_width-1] memory_bank[0:memory_bank_depth-1];
 
-reg [0:pointer_width-1] head_pointer_regfile[0:pointer_depth-1];
+reg [0:memory_addr_width-1] head_pointer_regfile[0:max_vc_number-1];
 
-reg [0:pointer_width-1] tail_pointer_regfile[0:pointer_depth-1];
+reg [0:memory_addr_width-1] tail_pointer_regfile[0:max_vc_number-1];
 
-reg [0:pointer_width-1] next_pointer_regfile[0:pointer_depth-1];
+reg [0:memory_addr_width-1] next_pointer_regfile[0:memory_bank_depth-1];
 
 
-// next_pointer_buffer
-wire [0:memory_bank_depth*pointer_width-1] wnext_pointer;
+// free buffer tracker
+wire [0:memory_addr_width-1] memory_bank_read_ptr;
+
+wire [0:memory_addr_width-1] memory_bank_write_ptr;
+
+free_buffer_tracker #(
+	.memory_bank_depth(memory_bank_depth),
+	.memory_bank_width(memory_bank_width))
+    tracker(
+	.clk(clk),
+	.reset(reset),
+	.next_available_slot(memory_bank_write_ptr),
+	.new_freed_slot(memory_bank_read_ptr),
+	.read_enable(read_enable),
+	.write_enable(write_enable),
+	.memory_bank_full(memory_bank_full),
+	.memory_bank_empty(memory_bank_empty));
+
+
+// memory write
+always @(posedge clk or negedge reset)
+	if (!reset)
+		memory_bank[memory_bank_write_ptr]<=0;
+	else if (write_enable)
+		memory_bank[memory_bank_write_ptr]<=flit_in;
+
+
+// tail pointer update
+always @(posedge clk or negedge reset)
+	if (!reset)
+		tail_pointer_regfile[vc_written_into]<=0;
+	else if (write_enable)
+		tail_pointer_regfile[vc_written_into]<=memory_bank_write_ptr;
+
+
+// next pointer update
+wire [0:memory_addr_width-1] wtail_pointer;
+wire [0:max_vc_number*memory_addr_width-1] group_tail_pointer;
 
 generate
 genvar item;
-for (item=0;item<memory_bank_depth;item=item+1)
-begin:anonymous1
-	assign wnext_pointer[item*pointer_width:(item+1)*pointer_width-1]=next_pointer_regfile[item];
+
+for (item=0;item<max_vc_number;item=item+1)
+begin:next_ptr
+	assign group_tail_pointer[item*memory_addr_width:(item+1)*memory_addr_width-1]=head_pointer_regfile[item];
 end
+
 endgenerate
 
-wire [0:pointer_width-1] next_ptr;
-
 c_select_1ofn #(
-	.num_ports(memory_bank_depth),
-	.width(pointer_width))
-    next_ptr_selector(
-	.select(read_ptr),
-	.data_in(wnext_pointer),
-	.data_out(next_ptr));
+	.num_ports(max_vc_number),
+	.width(memory_addr_width))
+    tail_ptr_selector(
+	.select(vc_written_into),
+	.data_in(group_tail_pointer),
+	.data_out(wtail_pointer));
+
+always @(posedge clk or negedge reset)
+	if (!reset)
+		next_pointer_regfile[wtail_pointer]<=0;
+	else if (write_enable)
+		next_pointer_regfile[wtail_pointer]<=memory_bank_write_ptr;
 
 
-// read from buffer
-wire [0:max_vc_number*pointer_width-1] whead_pointer;
+// head pointer update
+always @(posedge clk or negedge reset)
+	if (!reset)
+		head_pointer_regfile[vc_read_from]<=0;
+	else if (read_enable)
+		head_pointer_regfile[vc_read_from]<=next_pointer_regfile[memory_bank_read_ptr];
+
+
+// memory read
+wire [0:max_vc_number*memory_addr_width-1] whead_pointer;
 
 generate
 genvar vc;
 for (vc=0;vc<max_vc_number;vc=vc+1)
-begin:anonymous2
-	assign whead_pointer[vc*pointer_width:(vc+1)*pointer_width-1]=head_pointer_regfile[vc];
+begin:read_ptr
+	assign whead_pointer[vc*memory_addr_width:(vc+1)*memory_addr_width-1]=head_pointer_regfile[vc];
 end
 endgenerate
 
-wire [0:pointer_width-1] read_ptr;
-
 c_select_1ofn #(
 	.num_ports(max_vc_number),
-	.width(pointer_width))
+	.width(memory_addr_width))
     head_ptr_selector(
 	.select(vc_read_from),
 	.data_in(whead_pointer),
-	.data_out(read_ptr));
+	.data_out(memory_bank_read_ptr));
 
 always @(posedge clk or negedge reset)
-	if (~reset)
+	if (!reset)
 		flit_out <= 0;
-	else	
-		flit_out <= memory_bank[read_ptr];
-
-
-// write into buffer
-wire [0:max_vc_number*pointer_width-1] wtail_pointer;
-
-generate
-genvar vc;
-for (vc=0;vc<max_vc_number;vc=vc+1)
-begin:anonymous3
-	assign wtail_pointer[vc*pointer_width:(vc+1)*pointer_width-1]=tail_pointer_regfile[vc];
-end
-endgenerate
-
-wire [0:pointer_width-1] write_ptr;
-
-c_select_1ofn #(
-	.num_ports(max_vc_number),
-	.width(pointer_width))
-    tail_ptr_selector(
-	.select(vc_write_into),
-	.data_in(wtail_pointer),
-	.data_out(write_ptr));
-
-always @(posedge clk or negedge reset)
-	if (~reset)
-		memory_bank[write_ptr]<=0;
-	else
-		memory_bank[write_ptr]<=flit_in;
-
+	else if (read_enable)	
+		flit_out <= memory_bank[memory_bank_read_ptr];
 
 
 endmodule
--- DynVCRouter/testbench_free_buffer_tracker.v
+++ DynVCRouter/testbench_free_buffer_tracker.v
@@ -1,13 +1,12 @@
 `timescale 1ns/1ns
 
 
-module testbench();
+module testbench_free_buffer_tracker();
 
 
 `include "c_functions.v"
 
 
-parameter max_vc_number=2;
 parameter memory_bank_depth=32;
 parameter memory_bank_width=64;
 
@@ -17,14 +16,12 @@ localparam memory_addr_width=clogb(memory_bank_depth);
 
 reg clk;
 reg reset;
+
 reg read_enable;
 reg write_enable;
-reg [0:memory_bank_width-1] flit_in;
-reg [0:max_vc_number-1] vc_read_from;
-reg [0:max_vc_number-1] vc_written_into;
 
-wire [0:memory_bank_width-1] flit_out;
-
+reg [0:memory_addr_width-1] new_freed_slot;
+wire [0:memory_addr_width-1] next_available_slot;
 
 wire memory_bank_full;
 wire memory_bank_empty;
@@ -37,15 +34,17 @@ begin
 	clk=0;
 	reset=0;
 
-	flit_in=0;
 	read_enable=0;
 	write_enable=0;
-	vc_read_from=0;
-	vc_written_into=0;
+	new_freed_slot=0;
 
 	#1  reset=1;
 	#1  reset=0;
 	#1  reset=1;
+
+	read_enable=0;
+	write_enable=0;
+	new_freed_slot=0;
 
 	#100 $finish;
 	$display($time,"simulation finalized.\n");
@@ -60,32 +59,33 @@ $dumpvars;
 end
 
 
+initial
+begin
+	$monitor($time," memory_bank_full=%d, memory_bank_empty=%d, read_enable=%d, write_enable=%d, next_available_slot=%h, new_freed_slot=%h",memory_bank_full,memory_bank_empty,read_enable,write_enable,next_available_slot,new_freed_slot);
+end
+
+
 always
 	#1 clk=~clk;
 
 
-always
+always 
 begin
 	#1;
-	flit_in={$random}%100;
-	vc_read_from={$random}%2;
-	vc_written_into={$random}%2;
 	read_enable=memory_bank_empty?0:{$random}%5;
-	write_enable=memory_bank_full?0:{$random}%5;
+	write_enable=memory_bank_full?0:{$random}%10;
+	new_freed_slot={$random}%memory_bank_depth;
 end
 
 
-memory_bank #(
-	.max_vc_number(max_vc_number),
+free_buffer_tracker #(
 	.memory_bank_depth(memory_bank_depth),
 	.memory_bank_width(memory_bank_width))
-    bank(
+    tracker(
 	.clk(clk),
 	.reset(reset),
-	.flit_in(flit_in),
-	.vc_written_into(vc_written_into),
-	.flit_out(flit_out),
-	.vc_read_from(vc_read_from),
+	.next_available_slot(next_available_slot),
+	.new_freed_slot(new_freed_slot),
 	.read_enable(read_enable),
 	.write_enable(write_enable),
 	.memory_bank_full(memory_bank_full),
